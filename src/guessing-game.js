9c2237e889bba4780ff5ed52673d4d3643c590f1
Pavel Grudinskiy <mr.pv234@mail.ru>
2019-10-07 15:35:00 +0300

    completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.first = min;
+        this.second = max;
     }
 
     guess() {
-
+        return Math.round((this.first + this.second) / 2);
     }
 
     lower() {
-
+        this.second = this.guess();
     }
 
     greater() {
-
+        this.first = this.guess();
     }
 }
 
